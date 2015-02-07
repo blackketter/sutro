c959f0f19156b01e841666afa0bf1ed1a5a5a571
Dean Blackketter <dean@blackketter.com>
2015-02-07 15:54:24 -0800

    The custom raspistill version is not needed (i.e. we won't be using the lapse option retriggered every minute.)

--- snapshot.sh
+++ snapshot.sh
@@ -12,7 +12,7 @@ mkdir -p ${PHOTOSPREFIX}/${TODAY}
 JPG=${TODAY}/${TODAY}${TIME}
 LATEST=${PHOTOSPREFIX}/latest.jpg
 
-raspistill-custom -mm matrix -drc high -st -q 100 -h 1080 -w 1920 --brightness 50 -ISO 100 -n -hf -vf -v -o ${PHOTOSPREFIX}/${JPG}.jpg 
+raspistill -mm matrix -drc high -st -q 100 -h 1080 -w 1920 --brightness 50 -ISO 100 -n -hf -vf -v -o ${PHOTOSPREFIX}/${JPG}.jpg 
 #~/raspistill-dean -t 50000 -mm matrix -drc high -st -q 100 -h 1080 -w 1920  --brightness 50 -ISO 100 -n -hf -vf --timelapse 5000 -v  -o ${PHOTOSPREFIX}/${JPG}%02d.jpg -l ${LATEST}
 rm $LATEST
 ln -s ${PHOTOSPREFIX}/${JPG}.jpg $LATEST
