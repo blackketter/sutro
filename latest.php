5ec1409adb19108b192423dc8207ad03df8e7e67
Dean Blackketter <dean@blackketter.com>
2020-07-31 08:04:12 -0700

    Make medium images hourly, then delete them each night. Add latest.php

---
+++ latest.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Latest Sutro Lapse Images</title>
+<style type="text/css">
+	body { background-color: black; font: 14px/1.3 verdana, arial, helvetica, sans-serif; }
+	h1 { font-size:1.3em; }
+	h2 { font-size:1.2em; }
+	a:link { color:#33c; }
+	a:visited { color:#339; }
+	p { color:#888; }
+
+	/* so linked image won't have border */
+	a img { border:none; }
+</style>
+</head>
+
+<body>
+<p>Latest:
+<a href="http://zerow.local/lapse/latest.jpg"><img src="http://zerow.local/lapse/latest.jpg" width="100%"></a>
+<p>Yesterday: <a href="yesterday.log">(log)</a>
+<video controls autoplay muted loop width="100%"><source src="yesterday.mp4" /></video>
+<p>Grid:
+<a href="yesterday-grid.jpg"><img src="yesterday-grid.jpg" width="100%"></a>
+<p>Average:
+<a href="yesterday-mean.jpg"><img src="yesterday-mean.jpg" width="100%"></a>
+</body>
+</html>
