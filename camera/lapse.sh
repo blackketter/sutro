4c3373692a904cec83e462b5881cf405d49611df
Dean Blackketter <dean@blackketter.com>
2021-02-13 15:29:16 -0800

    cleanup

--- camera/lapse.sh
+++ camera/lapse.sh
@@ -5,36 +5,33 @@ YESTERDAY=$(date +"%Y%m%d" --date=yesterday)
 TWODAYSAGO=$(date +"%Y%m%d" --date="-2 days")
 
 TIME=$(date +"%H%M%S")
-PHOTOSPREFIX=/var/www/html/lapse
 
-cd ${PHOTOSPREFIX}
-
-mkdir -p ${TODAY}
+mkdir -p stills/${TODAY}
 
 echo "Taking lapse at ${TODAY} ${TIME}..."
 
-
-raspistill -t 5000 -ex night -mm spot -ifx denoise -q 15 -h 1080 -w 1920 -o lapse00.jpg
+raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse00.jpg
 sleep 3.5
-raspistill -t 5000 -ex night -mm spot -ifx denoise -q 15 -h 1080 -w 1920 -o lapse01.jpg
+raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse01.jpg
 sleep 3.5
-raspistill -t 5000 -ex night -mm spot -ifx denoise -q 15 -h 1080 -w 1920 -o lapse02.jpg
+raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse02.jpg
 sleep 3.5
-raspistill -t 5000 -ex night -mm spot -ifx denoise -q 15 -h 1080 -w 1920 -o lapse03.jpg
+raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse03.jpg
 sleep 3.5
-raspistill -t 5000 -ex night -mm spot -ifx denoise -q 15 -h 1080 -w 1920 -o lapse04.jpg
+raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse04.jpg
 sleep 3.5
-raspistill -t 5000 -ex night -mm spot -ifx denoise -q 15 -h 1080 -w 1920 -o lapse05.jpg
+raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse05.jpg
 
 for i in lapse*.jpg; do
     # Process $i
     D=`date -r ${i} +%Y%m%d%H%M%S`
 #   echo "Moving ${i} to ${D}.jpg"
-    mv ${i} ${TODAY}/${D}.jpg
+    mv ${i} stills/${TODAY}/${D}.jpg
 done
 
 rm latest.jpg
-ln -s ${TODAY}/${D}.jpg latest.jpg
 
-rm -rf $PHOTOSPREFIX/${TWODAYSAGO}
+ln -s stills/${TODAY}/${D}.jpg latest.jpg
+
+rm -rf stills/${TWODAYSAGO}
 
