cc40d040445d90859f273b68daa4b39e10d7c7ec
Dean Blackketter <dean@blackketter.com>
2021-02-13 15:39:11 -0800

    Move lapse images into lapse dir

--- camera/lapse.sh
+++ camera/lapse.sh
@@ -10,19 +10,19 @@ mkdir -p stills/${TODAY}
 
 echo "Taking lapse at ${TODAY} ${TIME}..."
 
-raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse00.jpg
+raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse/lapse00.jpg
 sleep 3.5
-raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse01.jpg
+raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse/lapse01.jpg
 sleep 3.5
-raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse02.jpg
+raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse/lapse02.jpg
 sleep 3.5
-raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse03.jpg
+raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse/lapse03.jpg
 sleep 3.5
-raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse04.jpg
+raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse/lapse04.jpg
 sleep 3.5
-raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse05.jpg
+raspistill -t 5000 -ex auto  -mm average -ifx denoise -q 15 -h 1080 -w 1920 -o lapse/lapse05.jpg
 
-for i in lapse*.jpg; do
+for i in lapse/lapse*.jpg; do
     # Process $i
     D=`date -r ${i} +%Y%m%d%H%M%S`
 #   echo "Moving ${i} to ${D}.jpg"
